a91c9d7ef291f3938424a7823453efc72aedc134
Hong Ma <hma@thoughtworks.com>
2017-10-26 10:22:23 +0800

    update TwsProgressCard

--- src/lib/components/tws-progress-card.js
+++ src/lib/components/tws-progress-card.js
@@ -1,9 +1,26 @@
 import React, { Component } from 'react'
+import { Card, Progress } from 'antd'
+import PropTypes from 'prop-types'
 
 export default class TwsProgressCard extends Component {
-  render() {
+  render () {
     return (
-      <div>TwsProgressCard</div>
+      <div>
+        <Card noHovering title={this.props.title} onClick={this.props.onClick}>
+          <Progress percent={this.props.percent}/>
+          {this.props.children}
+        </Card>
+      </div>
     )
   }
+}
+
+TwsProgressCard.defaultProps = {
+  percent: 0
+}
+
+TwsProgressCard.propTypes = {
+  title: PropTypes.string.isRequired,
+  percent: PropTypes.number.isRequired,
+  onClick: PropTypes.func
 }
