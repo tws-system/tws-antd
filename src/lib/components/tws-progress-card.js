5cb316fd806d285155ba887a8c9525a32423c4d2
Hong Ma <hma@thoughtworks.com>
2017-10-26 10:04:03 +0800

    fix component issue

--- src/lib/components/tws-progress-card.js
+++ src/lib/components/tws-progress-card.js
@@ -1,6 +1,6 @@
 import React, { Component } from 'react'
 
-export default class TwsProgressCard {
+export default class TwsProgressCard extends Component {
   render() {
     return (
       <div>TwsProgressCard</div>
