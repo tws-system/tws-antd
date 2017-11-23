e0834e38eba4c8c8703248d90d79bb68228e56b0
Hong Ma <hma@thoughtworks.com>
2017-11-23 10:18:04 +0800

    update tes-layout

--- src/lib/index.js
+++ src/lib/index.js
@@ -1,9 +1,11 @@
 import TwsMarkdownEditor from './components/tws-markdown-editor'
 import TwsProgressCard from './components/tws-progress-card'
 import TwsBelongTask from './components/tws-belong-task'
+import TwsLayout from './components/tws-layout'
 
 export {
   TwsMarkdownEditor,
   TwsProgressCard,
-  TwsBelongTask
+  TwsBelongTask,
+  TwsLayout
 };
