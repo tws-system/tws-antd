8db67f75de7f1293edda602be421c8bb3b7d8111
Hong Ma <hma@thoughtworks.com>
2017-11-30 17:25:56 +0800

    fix bug

--- src/lib/components/tws-edit-assignment-status/index.js
+++ src/lib/components/tws-edit-assignment-status/index.js
@@ -17,8 +17,8 @@ class EditStatusBox extends Component {
           {<Select style={{width: 120}} disabled={this.props.disabled} value={this.props.status} defaultValue={'未完成'}
                    onChange={this.props.onChangeStatus}>
             <Option value='优秀'>优秀</Option>
-            <Option value='完成'>完成</Option>
             <Option value='已完成'>已完成</Option>
+            <Option value='已提交'>已提交</Option>
             <Option value='未完成'>未完成</Option>
           </Select>}
         </Col>
