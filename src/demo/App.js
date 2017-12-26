4f636cb00e37c12b2b874f969cc651fda300bef4
Hong Ma <hma@thoughtworks.com>
2017-12-26 15:25:02 +0800

    fix style for show section

--- src/demo/App.js
+++ src/demo/App.js
@@ -12,7 +12,7 @@ class App extends Component {
         <TwsLayout userName='zhang三' lang='en' logo={logo} onChange={console.log} logoutUrl={'jjjfdsjk'} userCenterHomeUrl={'bbbbbbbb'}>
           {"vvvv"}
         </TwsLayout>
-        <TwsSection taskType="选修" topicTitle="topic" taskTitle="task" sectionDescription="description" color={"red"}/>
+        <TwsSection taskType="选修" topicTitle="topicsfjddddfasjkkkkkkkkdddddddddddddddddddddddddddl" taskTitle="task狂热我日风景库胸444444任务炯认为违法和体育房价大搜ID骄傲发嗲少放点 房间大搜IE日日日绕付军翁热豆腐军诶偶二奥无无付几苏多付付付发二多付话地所" sectionDescription="description到发死偶风二二二付晕或无求二二二付或一无二付多或撒与撒啊啊啊啊啊啊啊啊啊啊啊啊啊啊啊啊啊啊啊啊啊啊啊啊啊啊" color={"red"}/>
       </div>
     )
   }
