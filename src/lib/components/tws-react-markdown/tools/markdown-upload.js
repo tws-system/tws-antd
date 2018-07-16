c2d87ae48804ef9027eaeceb15f9d964a71242b2
aceysx <aceysx@163.com>
2018-07-16 18:19:45 +0800

    [Xin][#164]: Add the upload function in markdown.

--- src/lib/components/tws-react-markdown/tools/markdown-upload.js
+++ src/lib/components/tws-react-markdown/tools/markdown-upload.js
@@ -7,6 +7,13 @@ const MarkdownUpload = ({name, action, accept}) => {
         name: name || 'file',
         accept: accept || 'image/*',
         action,
+        beforeUpload(file){
+            const isLt2M = file.size / 1024 / 1024 < 2;
+            if (!isLt2M) {
+                message.error('Image must smaller than 2MB!');
+                return false
+            }
+        },
         onChange(info) {
             if (info.file.status === 'done') {
                 message.success(`${info.file.name} file uploaded successfully`);
@@ -15,6 +22,7 @@ const MarkdownUpload = ({name, action, accept}) => {
                 message.error(`${info.file.name} file upload failed.`);
             }
         },
+
     };
     return (
         <Upload {...props}>
