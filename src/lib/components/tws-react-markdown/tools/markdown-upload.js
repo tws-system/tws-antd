88fe5ab8afec38b6179eefa0ba27de8a08080d79
aceysx <aceysx@163.com>
2018-07-17 12:45:43 +0800

    [Xin][]: Modify upload function

--- src/lib/components/tws-react-markdown/tools/markdown-upload.js
+++ src/lib/components/tws-react-markdown/tools/markdown-upload.js
@@ -2,7 +2,7 @@ import React from 'react'
 import {Upload, message, Icon} from 'antd';
 
 
-const MarkdownUpload = ({name, action, accept}) => {
+const MarkdownUpload = ({name, action, accept, uploadImageSuccess}) => {
     const props = {
         name: name || 'file',
         accept: accept || 'image/*',
@@ -17,7 +17,7 @@ const MarkdownUpload = ({name, action, accept}) => {
         onChange(info) {
             if (info.file.status === 'done') {
                 message.success(`${info.file.name} file uploaded successfully`);
-                this.props.uploadImageSuccess(info)
+                uploadImageSuccess(info)
             } else if (info.file.status === 'error') {
                 message.error(`${info.file.name} file upload failed.`);
             }
