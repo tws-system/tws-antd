46d9812215f3dbbb0b15d295bf64df25ad1b0331
aceysx <aceysx@163.com>
2018-11-09 18:25:45 +0800

    Update English local.

--- src/lib/locales/es.messages.js
+++ src/lib/locales/es.messages.js
@@ -1,7 +1,12 @@
 const lang = {
-  '所属任务卡':'Task',
-  '分数':'Score',
-  '状态':'Status',
+    '所属任务卡':'Belongs to',
+    '分数':'Score',
+    '状态':'Status',
+    '暂无最新消息':'There is no notification.',
+    '查看全部':'load all',
+    '最新消息':'@Me',
+    '个人中心':'Profile',
+    '退出':'Logout',
 }
 
 export default lang
